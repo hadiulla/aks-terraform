b38f90faf6fa8b8ec689fd76b8ed0b16848e7928
Hadi <mohammedhadiulla7@gmail.com>
2021-02-22 12:40:37 +0530

    update

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ resource "azurerm_resource_group" "kubernetes-lab-rg" {
   location = "East US"
 }
 
+resource "azurerm_log_analytics_workspace" "kube-log-ws" {
+  name                = "kube-log-ws"
+  location            = azurerm_resource_group.kubernetes-lab-rg.location
+  resource_group_name = azurerm_resource_group.kubernetes-lab-rg.name
+  sku                 = "Free"
+  retention_in_days   = 7
+}
+
 resource "azurerm_kubernetes_cluster" "kube-lab-cluster" {
   name                = "aks-cluster-1"
   location            = azurerm_resource_group.kubernetes-lab-rg.location
@@ -17,6 +25,13 @@ resource "azurerm_kubernetes_cluster" "kube-lab-cluster" {
 
   identity {
     type = "SystemAssigned"
+  }
+
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.kube-log-ws.id
+    }
   }
 
   tags = {
