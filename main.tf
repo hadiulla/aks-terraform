98164ac230d5f3d37e15b3692777d8154ebac3bf
Hadi <mohammedhadiulla7@gmail.com>
2021-03-11 17:10:09 +0530

    Added istio

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_kubernetes_cluster" "kube-lab-cluster" {
 
   default_node_pool {
     name       = "default"
-    node_count = 3
+    node_count = 1 
     vm_size    = "Standard_D2_v2"
   }
 
@@ -45,4 +45,93 @@ output "client_certificate" {
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.kube-lab-cluster.kube_config_raw
+}
+
+################## ISTIO CONFIG #########################
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
+data "azurerm_subscription" "current" {
+}
+
+resource "local_file" "kube_config" {
+  content    = azurerm_kubernetes_cluster.kube-lab-cluster.kube_admin_config_raw
+  filename   = ".kube/config"   
+}
+
+
+resource "null_resource" "set-kube-config" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = "az aks get-credentials -n ${azurerm_kubernetes_cluster.kube-lab-cluster.name} -g ${azurerm_resource_group.rg.name} --file \".kube/${azurerm_kubernetes_cluster.kube-lab-cluster.name}\" --admin --overwrite-existing"
+  }
+  depends_on = [local_file.kube_config]
+}
+
+
+resource "kubernetes_namespace" "istio_system" {
+  provider = kubernetes.local
+  metadata {
+    name = "istio-system"
+  }
+}
+
+resource "kubernetes_secret" "grafana" {
+  provider = kubernetes.local
+  metadata {
+    name      = "grafana"
+    namespace = "istio-system"
+    labels = {
+      app = "grafana"
+    }
+  }
+  data = {
+    username   = "admin"
+    passphrase = random_password.password.result
+  }
+  type       = "Opaque"
+  depends_on = [kubernetes_namespace.istio_system]
+}
+
+resource "kubernetes_secret" "kiali" {
+  provider = kubernetes.local
+  metadata {
+    name      = "kiali"
+    namespace = "istio-system"
+    labels = {
+      app = "kiali"
+    }
+  }
+  data = {
+    username   = "admin"
+    passphrase = random_password.password.result
+  }
+  type       = "Opaque"
+  depends_on = [kubernetes_namespace.istio_system]
+}
+
+resource "local_file" "istio-config" {
+  content = templatefile("${path.module}/istio-aks.tmpl", {
+    enableGrafana = true
+    enableKiali   = true
+    enableTracing = true
+  })
+  filename = ".istio/istio-aks.yaml"
+}
+
+resource "null_resource" "istio" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    command = "istioctl manifest apply -f \".istio/istio-aks.yaml\" --kubeconfig \".kube/${azurerm_kubernetes_cluster.kube-lab-cluster.name}\""
+  }
+  depends_on = [kubernetes_secret.grafana, kubernetes_secret.kiali, local_file.istio-config]
 }
